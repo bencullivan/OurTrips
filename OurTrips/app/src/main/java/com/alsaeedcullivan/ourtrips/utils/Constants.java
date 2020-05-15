2405ef32f04fbbac8a77cdc280d7ef98c4815be0
Benjamin Cullivan <bencullivan@Benjamins-MacBook-Pro-4.local>
2020-05-15 11:45:30 -0400

    Updated Constants

--- OurTrips/app/src/main/java/com/alsaeedcullivan/ourtrips/utils/Constants.java
+++ OurTrips/app/src/main/java/com/alsaeedcullivan/ourtrips/utils/Constants.java
@@ -1,12 +1,12 @@
 package com.alsaeedcullivan.ourtrips.utils;
 
+/**
+ * This is a class to contain the public constants used throughout the app
+ */
 public class Constants {
 
-    /**
-     * Cloud FireStore Database paths
-     */
+    // Cloud FireStore database paths
     public static final String USERS_PATH = "users/";
     public static final String TRIPS_PATH = "trips/";
 
-    
 }
